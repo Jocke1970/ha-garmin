335a1365f69129d72114f9f320d04a1709e1bb73
Joachim Eriksson <sa0azy@gmail.com>
2026-09-03 19:16:00 +0200

    feat: expose CTL ATL TSB engine

--- src/ha_garmin/fitness/__init__.py
+++ src/ha_garmin/fitness/__init__.py
@@ -6,14 +6,17 @@ from .load import (
     normalize_activities,
     normalize_activity,
 )
-from .models import ActivityMetrics, DailyLoad, GarminLoadCoverage
+from .models import ActivityMetrics, DailyLoad, GarminLoadCoverage, TrainingLoadPoint
+from .training import compute_ctl_atl_tsb
 
 __all__ = [
     "ActivityMetrics",
     "DailyLoad",
     "GarminLoadCoverage",
+    "TrainingLoadPoint",
     "analyze_garmin_load_coverage",
     "build_daily_garmin_load_series",
+    "compute_ctl_atl_tsb",
     "normalize_activities",
     "normalize_activity",
 ]
