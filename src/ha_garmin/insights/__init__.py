704c39f289aaf12d7734f5bf3462e822e34146ad
Joachim Eriksson <sa0azy@gmail.com>
2026-09-05 21:16:33 +0200

    Insights: export snapshot API

--- src/ha_garmin/insights/__init__.py
+++ src/ha_garmin/insights/__init__.py
@@ -1,9 +1,21 @@
 """Reusable Garmin Insights input models and normalizers."""
 
-from .models import DailyRecoveryMetrics
+from .models import (
+    DailyRecoveryMetrics,
+    InsightDataQuality,
+    InsightSnapshot,
+    LoadFocusSnapshot,
+    TrainingSnapshot,
+)
 from .recovery import build_daily_recovery_metrics
+from .snapshot import build_insight_snapshot
 
 __all__ = [
     "DailyRecoveryMetrics",
+    "InsightDataQuality",
+    "InsightSnapshot",
+    "LoadFocusSnapshot",
+    "TrainingSnapshot",
     "build_daily_recovery_metrics",
+    "build_insight_snapshot",
 ]
