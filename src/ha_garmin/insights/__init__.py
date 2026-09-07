729e1316466784682881b00b6e114ef08ff845ea
Joachim Eriksson <sa0azy@gmail.com>
2026-09-07 10:39:52 +0200

    Insights: expose Rules V1 result API

--- src/ha_garmin/insights/__init__.py
+++ src/ha_garmin/insights/__init__.py
@@ -1,21 +1,32 @@
-"""Reusable Garmin Insights input models and normalizers."""
+"""Reusable Garmin Insights inputs, rules and result contracts."""
 
 from .models import (
     DailyRecoveryMetrics,
+    InsightConfidence,
     InsightDataQuality,
+    InsightEvidence,
+    InsightResult,
+    InsightSeverity,
     InsightSnapshot,
     LoadFocusSnapshot,
     TrainingSnapshot,
 )
 from .recovery import build_daily_recovery_metrics
+from .rules import INSIGHT_RULESET_VERSION, evaluate_insights
 from .snapshot import build_insight_snapshot
 
 __all__ = [
     "DailyRecoveryMetrics",
+    "INSIGHT_RULESET_VERSION",
+    "InsightConfidence",
     "InsightDataQuality",
+    "InsightEvidence",
+    "InsightResult",
+    "InsightSeverity",
     "InsightSnapshot",
     "LoadFocusSnapshot",
     "TrainingSnapshot",
     "build_daily_recovery_metrics",
     "build_insight_snapshot",
+    "evaluate_insights",
 ]
